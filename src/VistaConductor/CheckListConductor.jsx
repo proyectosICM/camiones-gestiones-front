67de845a9857d45da3bcaace1be79e54b74f3b1e
Eduardo Aguilar <es123y123@gmail.com>
2023-07-21 16:40:17 -0500

    TablaIterativaConCambios

--- src/VistaConductor/CheckListConductor.jsx
+++ src/VistaConductor/CheckListConductor.jsx
@@ -1,72 +1,108 @@
-import React from "react";
+import React, { useState } from "react";
 import { Tabla } from "../Common/CheckList/Tabla";
+import { Button } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 
 export function CheckListConductor() {
+  const navigate = useNavigate();
+  const [currentTable, setCurrentTable] = useState(0);
 
-  const llantas = [
-    "Revisar ajuste",
-    "Cortes y averias",
-    "Revisar Presion recomendada",
-  ];
+  const tables = [
+    {
+      titulo: "Llantas",
+      datos: [
+        "Revisar ajuste",
+        "Cortes y averias",
+        "Revisar Presion recomendada",
+      ],
+    },
+    {
+      titulo: "Motor",
+      datos: [
+        "Niveles de Motor",
+        "Sistema de lubricacion de fugas",
+        "Sistema de combustible",
+      ],
+    },
+    {
+      titulo: "Sistema Electrico",
+      datos: [
+        "Luces",
+        "Sistema de carga",
+        "Mandos tablero",
+        "Sistema de arranque",
+        "Ruidos anormales",
+        "Otros equipos electricos",
+      ],
+    },
+    {
+      titulo: "Trasmision",
+      datos: [
+        "Embrague",
+        "Caja de cambio",
+        "Diferencial",
+        "Cardanes",
+        "Ruidos Anormales",
+      ],
+    },
+    {
+      titulo: "Direccion",
+      datos: [
+        "Seryo",
+        "Alineamiento",
+        "Pines, bocinas, terminales",
+        "Caja de Direcion",
+      ],
+    },
+    {
+      titulo: "Frenos",
+      datos: [
+        "Limpieza y regulacion",
+        "Presion de aire",
+        "Freno de estacionamiento",
+      ],
+    },
+    {
+      titulo: "Suspension",
+      datos: [
+        "Muelles, Bolsas de aire",
+        "Amortiguadores",
+        "Ejes barra estabilizadora",
+      ],
+    },
+    {
+      titulo: "Cabina",
+      datos: [
+        "Carroceria: Parabrisas, puertas, chapas, asientos",
+        "Chasis: Tornamesas, Bastidor",
+      ],
+    },
+  ]; 
+  
+  const handleDetails = () => {
+    navigate('/camion-detalle');
+  }
 
-  const motor = [
-    "Niveles de Motor",
-    "Sistema de lubricacion de fugas",
-    "Sistema de combustible",
-  ];
+  const handleBack = () => {
+    if (currentTable > 0) {
+      setCurrentTable(currentTable - 1);
+    }
+  };
 
-  const sisElectrico = [
-    "Luces  ",
-    "Sistema de carga",
-    "Mandos tablero",
-    "Sistema de arranque",
-    "Ruidos anormales",
-    "Otros equipos electricos"
-  ];
-
-  const trasmision = [
-    "Embrague",
-    "Caja de cambio",
-    "Diferencial",
-    "Cardanes",
-    "Ruidos Anormales"
-  ];
-
-  const direccion = [
-    "Seryo",
-    "Alineamiento",
-    "Pines, bocinas, terminales",
-    "Caja de Direcion"
-  ];
-
-  const frenos = [
-    "Limpieza y regulacion",
-    "Presion de aire",
-    "Freno de estacionamiento"
-  ];
-
-  const suspension = [
-    "Muelles, Bolsas de aire",
-    "Amortiguadores",
-    "Ejes barra estabilizadora"
-  ];
-
-  const cabina = [
-    "Carroceria: Parabrisas, puertas, chapas, asientos",
-    "Chasis: Tornamesas, Bastidor"
-  ]
+  const handleNext = () => {
+    if (currentTable < tables.length - 1) {
+      setCurrentTable(currentTable + 1);
+    }
+  };
 
   return (
     <>
+      <Button onClick={handleDetails}>Atras</Button>
       <h1>Marque si esta en buen o en mal estado:</h1>
-      <Tabla titulo="Llantas" datos={llantas} />
-      <Tabla titulo="Motor" datos={motor} />
-      <Tabla titulo="Sistema Electrico" datos={sisElectrico} />
-      <Tabla titulo="Trasmision" datos={trasmision} />
-      <Tabla titulo="Direccion" datos={direccion} />
-      <Tabla titulo="Frenos" datos={frenos} />
-      <Tabla titulo="Suspension" datos={suspension} />
-      <Tabla titulo="Cabina" datos={cabina} />
+      <Tabla titulo={tables[currentTable].titulo} datos={tables[currentTable].datos} />
+      <Button onClick={handleBack} disabled={currentTable === 0}>Anterior</Button>
+      <Button onClick={handleNext} disabled={currentTable === tables.length - 1}>Siguiente</Button>
+
     </>
   );
 }
--- src/test/CheckListConductor.test.jsx
+++ src/test/CheckListConductor.test.jsx
@@ -1,9 +1,13 @@
 import { render } from "@testing-library/react";
 import { CheckListConductor } from "../VistaConductor/CheckListConductor";
 
-describe('CheckListConductor', () => {
-    test('Verificar renderizado del CheckList', () => {
-        const { getByText } = render(<CheckListConductor />);
-        expect(getByText('Reporte de conductor')).toBeInTheDocument();
-    });
-});
+jest.mock("react-router-dom", () => ({
+  useNavigate: () => jest.fn(),
+}));
+
+describe("CheckListConductor", () => {
+  test("Verificar renderizado del CheckList", () => {
+    const { getByText } = render(<CheckListConductor />);
+    expect(getByText("Marque si esta en buen o en mal estado:")).toBeInTheDocument();
+  });
+}); 
