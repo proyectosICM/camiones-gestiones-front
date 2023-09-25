d94499332314a3abba931f82a05c72e8e8086bdd
Eduardo Aguilar <es123y123@gmail.com>
2023-09-25 17:22:22 -0500

    Nose

--- src/CLCamiones.jsx
+++ src/CLCamiones.jsx
@@ -1,7 +1,25 @@
-import React from 'react'
-import { useLocation } from 'react-router-dom';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
 
 export function ClCamiones(){
+    const [datos, setDatos] = useState()
+    
+    const Listar = async () => {
+        try {
+            const response = await axios.get('http://localhost:8080/api/carriles');
+            setDatos(response.data);
+        } catch (error) {
+            console.error("Error al obtener los datos:", error);
+        }
+    }
+
+    useEffect(()=> {
+        Listar();
+    },[])
+
+    useEffect(() => {
+        console.log(datos); // Colocar el console.log aquí
+    }, [datos]);
 
     return(
         <>
